d11316a119a637a6732f8a69b0c3d1c9e987eaca
TrideepLD <trideeplaldas@hotmail.com>
2021-02-14 21:37:39 +1100

    Added Constructor and State to Square Component

--- JavaScript_Frameworks/ReactJS/my-app/src/index.js
+++ JavaScript_Frameworks/ReactJS/my-app/src/index.js
@@ -3,15 +3,25 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
-    render() {
-      return (
-        // <button className="square" onClick={function() { alert('click'); }}>
-        // This change is done to avoid the confusing behaviour of this: https://yehudakatz.com/2011/08/11/understanding-javascript-function-invocation-and-this/
-        <button className="square" onClick={() => alert('click')}>
+
+  // Initialize constructor class
+  constructor(props) {
+    super(props); // Exists cause you need a super to define constructors in JS
+    // therefore all component classes with a constructor should have a super(props) call
+    this.state = {
+      value: null,
+    };
+  }
+
+  render() {
+    return (
+      // <button className="square" onClick={function() { alert('click'); }}>
+      // This change is done to avoid the confusing behaviour of this: https://yehudakatz.com/2011/08/11/understanding-javascript-function-invocation-and-this/
+      <button className="square" onClick={() => alert('click')}>
         {this.props.value} 
-        </button>
-        // Changed Square's render method to show the value in line 17.
-        // Button Tag changed so that it fills Square component with X when clicked.
+      </button>
+      // Changed Square's render method to show the value in line 17.
+      // Button Tag changed so that it fills Square component with X when clicked.
       );
     }
   }
